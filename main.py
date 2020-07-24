e753aa68a43dea0b603e22b32f62c92599b1b5ac
JalenBullock <JalenB846@gmail.com>
2020-07-24 16:57:10 +0000

    More code

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-for i range(j)
+for I Range = (i)
 loop | i = 0
+loop | i = 1
+loop | i = 2
+loop | i = 3
+loop | i = 4
+
+Outro loop | i = 0
+
+loop | i = 0
+loop | i = 1
+loop | i = 2
+loop | i = 3
+loop | i = 4
